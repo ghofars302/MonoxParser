412d025d44b34befd0d4038e4f9a9bbc0699f763
Monodoxy <ghofarsuprapto@gmail.com>
2018-08-02 14:25:02 +0700

    Update index.ts

--- index.ts
+++ index.ts
@@ -30,10 +30,10 @@ class MonoxParser {
     
     this.app.post('/eval', async (req: express.Request, res: express.Response) => {
       console.log(req)
-      if (!req.headers['Authorization'] || ![process.env.TOKEN1].includes(req.headers['Authorization'])) return res.status(401).send('Invalid Authorization token')
+      if (!req.headers['authorization'] || ![process.env.TOKEN1].includes(req.headers['authorization'])) return res.status(401).send('Invalid Authorization token')
       let bodyText;
      
-      if (req.headers['Content-Type'] === 'application/json') {
+      if (req.headers['content-type'] === 'application/json') {
           bodyText = req.body ? (req.body.text ? req.body.text : '') : undefined;
       }
       
